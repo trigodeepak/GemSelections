91a3c1af47d979b8d336517de7171ee3284ee8fe
Nikhil <nikhilyadav3000@gmail.com>
2017-06-16 01:10:24 +0530

    images added and home page started

--- app/src/main/java/com/example/nikhil3000/gemselections/MainActivity.java
+++ app/src/main/java/com/example/nikhil3000/gemselections/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.nikhil3000.gemselections;
 
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ImageView cover_pic = (ImageView)findViewById(R.id.iv_home_);
+        try {
+            InputStream in = getAssets().open("images/Gemselection.jpg");
+            Drawable drawable = Drawable.createFromStream(in,null);
+            cover_pic.setImageDrawable(drawable);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
