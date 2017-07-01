9d8f40798f927a949c1595f81c3620343ea4034a
Himanshu Nain <himanshu_bt2k16@dtu.ac.in>
2017-07-01 11:34:23 +0530

    BugFix#2

--- app/src/main/java/com/example/nikhil3000/gemselections/MainActivity.java
+++ app/src/main/java/com/example/nikhil3000/gemselections/MainActivity.java
@@ -271,6 +271,7 @@ public class MainActivity extends AppCompatActivity
                         .setNegativeButton("No", new DialogInterface.OnClickListener() {
                             @Override
                             public void onClick(DialogInterface dialog, int which) {
+                                display_selected_item(R.id.nav_home);
                                 dialog.dismiss();
                             }
                         })
